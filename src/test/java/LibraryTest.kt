b60c25d25b951de677f8b1f6d6319c08b786dca8
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2016-05-07 13:43:04 -0400

    Convert java to kotlin

---
+++ src/test/java/LibraryTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+class LibraryTest {
+    @Test fun testSomeLibraryMethod() {
+        val classUnderTest = Library()
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod())
+    }
+}
